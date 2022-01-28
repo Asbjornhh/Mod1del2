3bbec66b27e5ff117756333363572d9598e2280f
Asbjornhh <89381324+Asbjornhh@users.noreply.github.com>
2022-01-28 10:03:09 +0100

    first commit

---
+++ Main.go
@@ -0,0 +1,14 @@
+package main
+
+import "test/myquote"
+
+import (
+	"github.com/Asbjornhh/Mod1del2"
+)
+
+
+func ()  {
+
+	myquote.Myquote()
+
+}
---
+++ myquote/myquote.go
@@ -0,0 +1,14 @@
+package myquote
+
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func Myquote() {
+
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+}
